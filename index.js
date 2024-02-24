3ae198b5d2f514fab6b13acdf00b17b053a34cc8
PrateekTechmatys <prateeky562@gmail.com>
2024-02-24 13:36:48 +0530

    done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
     const imageTag = document.querySelector('#ImageId');
     const input_text = document.querySelector('#input_text').value;
     const h1text = document.querySelector('.yoyo');
+    const last = document.querySelector('.last');
     console.log(input_text);
     const calling_Api = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input_text}`);
 
@@ -14,6 +15,7 @@
         const responseData = await calling_Api.blob();
         const imageUrl = URL.createObjectURL(responseData);
         imageTag.src = imageUrl;
+        last.style.display = 'flex'; 
     } else {
         console.error('Failed to fetch QR code image');
         h1text.classList.add('textt');
